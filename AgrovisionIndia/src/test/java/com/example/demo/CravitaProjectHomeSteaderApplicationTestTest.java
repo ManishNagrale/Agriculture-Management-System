489bab18d87f0f30a5af7c7888c658f7117331dd
ManishNagrale <manishnagrale22@gmail.com>
2026-09-14 15:59:14 +0530

    Final Commit

---
+++ AgrovisionIndia/src/test/java/com/example/demo/CravitaProjectHomeSteaderApplicationTestTest.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CravitaProjectHomeSteaderApplicationTestTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
